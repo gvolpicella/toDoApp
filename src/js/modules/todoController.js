141512d022fc03d64424e18e65bff9b899cf39ba
Giuseppe Volpicella <g.volpicella@earnaway.com>
2016-12-04 11:44:58 +0000

    bug fix on sorting compare function

--- src/js/modules/todoController.js
+++ src/js/modules/todoController.js
@@ -85,7 +85,8 @@ var todoController = (function() {
 		date1 = new Date(a.date);
 		date2 = new Date(b.date);
 		if (date1 < date2) { return -1; }
-		else return 1;
+		else if (date1 > date2) { return 1; }
+		else return 0;
 	};	
 
 	/**
